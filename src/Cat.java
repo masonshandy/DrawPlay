ff75d291824357dba8b99d079fcc6abfb4299905
masonshandy <103527242+masonshandy@users.noreply.github.com>
2023-02-17 11:12:53 -0700

    changed the head color to cyan

--- src/Cat.java
+++ src/Cat.java
@@ -3,7 +3,7 @@ import java.awt.*;
 import java.awt.geom.*;
 
 public class Cat {
-	// drawing constants are private - noone needs to know what we're doing
+	// drawing constants are private - no one needs to know what we're doing
 	// pick a head dimension
 	private static final int HEAD_DIMENSION = 100;
 	// eyes will be about 1/4 from top of head and 1/4 from left
@@ -27,7 +27,7 @@ public class Cat {
 		int x=catX;
 		int y=catY;
 		// Draw the head
-		g2.setColor(Color.black);
+		g2.setColor(Color.cyan);
 		g2.fillOval(x, y, HEAD_DIMENSION, HEAD_DIMENSION);
 		// Draw the eyes
 		g2.setColor(Color.blue);
@@ -41,7 +41,7 @@ public class Cat {
 		x = catX + MOUTH_X;
 		y = catY + MOUTH_Y;
 		g2.fillOval(x, y, MOUTH_WIDTH, MOUTH_HEIGHT);
-		g2.setColor(Color.black);
+		g2.setColor(Color.cyan);
 		// Meow text appears below cat head, +10 places below 
 		// so it doesn't overlap the drawing
 		g2.drawString("Meow", catX, catY+HEAD_DIMENSION+10);	
